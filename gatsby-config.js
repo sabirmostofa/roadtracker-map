7698068dadd51b11502a0e2850f3c5ce3e7be612
sabirmostofa <sabirmostofa@gmail.com>
2021-02-14 20:56:00 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,27 @@
-const config = require("./package.json");
+const config = require( './package.json' );
 
 const { title, description, author, repository, homepage } = config;
 
 const siteMetadata = {
   companyName: title,
-  companyUrl: "http://localhost",
-  authorName: "",
-  authorUrl: "",
-  siteUrl: "http://localhost",
+  companyUrl: repository.url,
+  authorName: author.name,
+  authorUrl: author.url,
+  siteUrl: homepage,
   siteDescription: description,
 };
 
 module.exports = {
   siteMetadata,
   plugins: [
-    "gatsby-plugin-resolve-src",
+    'gatsby-plugin-resolve-src',
     {
-      resolve: "gatsby-plugin-sass",
+      resolve: 'gatsby-plugin-sass',
       options: {
-        implementation: require("sass"),
+        implementation: require( 'sass' ),
       },
     },
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -29,14 +29,14 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
-    "gatsby-plugin-react-leaflet",
+    'gatsby-plugin-react-leaflet',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: siteMetadata.companyName,
         short_name: siteMetadata.companyName,
-        start_url: "/",
-        icon: "src/assets/images/react-leaflet-icon.png",
+        start_url: '/',
+        icon: 'src/assets/images/react-leaflet-icon.png',
       },
     },
   ],
